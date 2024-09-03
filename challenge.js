558da05b38163206d25c15345785fe4bbad86c64
Abdulkadir <119424677+Abdulkadir1614@users.noreply.github.com>
2024-09-03 09:36:02 +0800

    Done exercise

--- challenge.js
+++ challenge.js
@@ -1,2 +1,22 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+let A = 84;
+let B = 20;
+let C = 100
+
+if(A > B  && A > C){
+    console.log("The largest out of the three number is %d", A);
+}
+
+else if(B > A && B > C ){
+    console.log("The largest out of the three number is %d", B);
+}
+
+else if(C > A  && C > B){
+    console.log("The largest out of the three number is %d", C);
+}
+
+else {
+    console.log ("Something went wrong, pls retry!");
+}
